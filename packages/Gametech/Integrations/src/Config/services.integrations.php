7342195ca9dbce480b2f48a39968d6d6721ed3fc
b0855626503 <b0855626503@gmail.com>
2025-10-28 00:10:00 +0700

    Update services.integrations.php

--- packages/Gametech/Integrations/src/Config/services.integrations.php
+++ packages/Gametech/Integrations/src/Config/services.integrations.php
@@ -2,11 +2,11 @@
 
 return [
     'vegus' => [
-        'username' => env('VEGUS_USERNAME', 'vegusapi'),
-        'password' => env('VEGUS_PASSWORD','vegus168@iap#P@ssw0rd884@!fktxs'),
+        'username' => env('VEGUS_USERNAME', ''),
+        'password' => env('VEGUS_PASSWORD',''),
     ],
     'kraken' => [
-        'key' => env('KRAKEN_KEY','2b6e6699-1a5c-448c-a5b8-47d1038eb2b4'),
+        'key' => env('KRAKEN_KEY',''),
     ],
     // เพิ่มคีย์ของ provider อื่น ๆ ที่จำเป็นในอนาคต
 ];
